6c5c892cd92ed868e3b1dc7e982557a349a57911
edgarsj <jekabsons.edgars@gmail.com>
2011-07-31 16:01:54 +0300

    Logo fix, bug fix when determining category_id, raised logo up a bit

--- app/controllers/furniture_controller.rb
+++ app/controllers/furniture_controller.rb
@@ -3,7 +3,7 @@ class FurnitureController < ApplicationController
   
   def index
     @furniture = Furniture.find_for_index(params)
-    @category = Category.find(params[:category_id])
+    @category = Category.find(params[:category_id])  if params[:category_id]
     @sub_category = SubCategory.find(params[:sub_category_id]) if params[:sub_category_id]
   end
   
