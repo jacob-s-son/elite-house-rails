83a2614ffe90ece1e4c71381281cded63a1b7400
EdgarsJ <jekabsons.edgars@gmail.com>
2011-08-05 19:16:57 +0300

    Updated deploy file, updated layout

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # RVM bootstrap
 $:.unshift(File.expand_path("~/.rvm/lib"))
 require 'rvm/capistrano'
-set :rvm_ruby_string, '1.8.7'
+set :rvm_ruby_string, '1.8.7@elite-house'
 set :rvm_type, :user
 
 # stages
