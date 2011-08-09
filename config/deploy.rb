2c2a1045f15ab909ed89a343d954bd1313502519
edgarsj <jekabsons.edgars@gmail.com>
2011-08-10 01:51:36 +0300

    Deleted branch select, removed DB copy after code update

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,6 @@ set :use_sudo, false
 set :scm, :git
 set :scm_username, "passenger"
 set :repository, "git@github.com:jacob-s-son/elite-house-rails.git"
-set :branch, "develop"
 set :git_enable_submodules, 1
 
 # rails env
@@ -68,6 +67,6 @@ namespace :deploy do
   end
 end
 
-after 'deploy:update_code', 'deploy:copy_db', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:symlink_shared'
 
 
