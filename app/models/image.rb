41b487c2e1d5177ebd4bdb1bca3ab473bf30b64d
Edgars Jēkabsons <jekabsons.edgars@gmail.com>
2012-04-09 13:01:10 +0300

    Added removing main flag for older pictures, when new is set

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
+  before_save :remove_main_flag
   named_scope :main, :conditions => ["main = ?", true]
   has_attached_file :picture, 
                     :styles => { :large => "500x500>",
@@ -7,5 +8,11 @@ class Image < ActiveRecord::Base
   
   def url(size)
     picture.url(size)
+  end
+  
+  private
+  
+  def remove_main_flag
+    Image.where(:furniture_id => furniture_id, :main => true).each { |f| f.update_attribute( "main", false ) } if main
   end
 end
